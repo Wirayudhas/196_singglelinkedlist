7e0e9108d025376d0b68daa7f87ca084e7962e76
Wirayudhas <arturwirayuda@gmail.com>
2024-04-25 11:12:08 +0700

    Menampilkan output ketika list kosong

--- 196_singglelinkedlist/196_singglelinkedlist.cpp
+++ 196_singglelinkedlist/196_singglelinkedlist.cpp
@@ -86,5 +86,20 @@ bool listEmpty() {
 
 }
 
+void travarse() {
+	if (listEmpty()) {
+		cout << "List kosong" << endl;
+		system("pause");
+		system("cls");
+		return;
+	}
+	else {
+		Node* currentNode = START;
+		while (currentNode != NULL) {
+			cout << "NIM: " << currentNode->noMhs << ", Nama: " << currentNode->name << endl;
+			currentNode = currentNode->next;
+		}
+	}
+}
 
 
